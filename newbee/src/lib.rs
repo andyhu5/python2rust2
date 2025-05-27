e8aecfbdc361af2847ca6dc704f67999e6cac80b
Your Name <you@example.com>
2025-05-27 21:47:02 +0800

    upate lib.rs and lib test

--- newbee/src/lib.rs
+++ newbee/src/lib.rs
@@ -1,4 +1,4 @@
-use pyo3::prelude::*;
+use pyo3::{exceptions::PyValueError, prelude::*};
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -11,10 +11,50 @@ fn hello() -> PyResult<String> {
     Ok("Hello, world!".to_string())
 }
 
+#[pyclass]
+struct Counter {
+    value:i32,
+}
+
+#[pymethods]
+impl Counter {
+    #[new]
+    fn new(value:i32) -> Self {
+        Counter { value }
+    }
+    fn increment(&mut self) {
+        self.value += 1;
+    }
+    fn get_value(&self) -> i32 {
+        self.value
+    }
+}
+
+#[pyfunction]
+fn divide(a: f64, b: f64) -> PyResult<f64> {
+    if b == 0.0 {
+        return Err(PyValueError::new_err("division by zero"))
+    } else {
+        Ok(a / b)
+    }
+}
+
+#[pyfunction]
+fn call_python_function(value:i32)-> PyResult<i32> {
+    Python::with_gil(|py| {
+        let math = py.import("math")?;
+        let result = math.getattr("floor")?.call1((value,))?.extract::<i32>()?;
+        Ok(result)
+    })
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn newbee(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(hello, m)?)?;
+    m.add_class::<Counter>()?;
+    m.add_function(wrap_pyfunction!(divide, m)?)?;
+    m.add_function(wrap_pyfunction!(call_python_function, m)?)?;
     Ok(())
 }
--- use_newbee/main.py
+++ use_newbee/main.py
@@ -3,5 +3,22 @@ import newbee
 def main():
     print(newbee.sum_as_string(1,2))
     print(newbee.hello())
-if __name__ == "__main__":  
+    
+    c = newbee.Counter(10)
+    c.increment()
+    c.increment()
+    print(c.get_value())
+    
+    try:
+        print(newbee.divide(10, 2))
+    except ZeroDivisionError as e:
+        print(f"Error: {e}")
+
+    try:
+        print(newbee.call_python_function(6))
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
     main()
